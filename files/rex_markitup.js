e765e57f3258556ce7ee92264292e566c17dc6f2
jdlx <github@rexdev.de>
2013-07-04 01:14:40 +0200

    incorporate selection.js - first steps..

--- files/rex_markitup.js
+++ files/rex_markitup.js
@@ -7,54 +7,58 @@
  * @package redaxo 4.4.x/4.5.x
  */
 
-var insertFileLink = function(file){
-  jQuery.markItUp({
-    openWith:'"',
-    closeWith:'":'+file,
-    placeHolder:file
-  });
-};
 
-var insertLink = function(url,desc){
-  jQuery.markItUp({
-    openWith:'"',
-    closeWith:'":'+url,
-    placeHolder:desc
-  });
-};
+jQuery(function($){ ////////////////////////////////////////////////////////////
 
-var insertImage = function(src, desc){
-  // jQuery.markItUp({replaceWith:"!./"+ src +"!"});
-  img = src.replace(/files\//, "");
-  jQuery.markItUp({
-    replaceWith:"!index.php?rex_resize=[![Image Width]!]w__"+ img +"!"
+  var insertFileLink = function(file){
+    $.markItUp({
+      openWith:'"',
+      closeWith:'":'+file,
+      placeHolder:file
     });
-};
+  };
 
-var markitup_getURLParam = function(strParamName){
-  var strReturn = "";
-  var strHref = window.location.href;
-  if ( strHref.indexOf("?") > -1 ){
-    var strQueryString = strHref.substr(strHref.indexOf("?")).toLowerCase();
-    var aQueryString = strQueryString.split("&");
-    for ( var iParam = 0; iParam < aQueryString.length; iParam++ ){
-      if( aQueryString[iParam].indexOf(strParamName.toLowerCase() + "=") > -1 ){
-        var aParam = aQueryString[iParam].split("=");
-        strReturn = aParam[1];
-        break;
+  var insertLink = function(url,desc){
+    $.markItUp({
+      openWith:'"',
+      closeWith:'":'+url,
+      placeHolder:desc
+    });
+  };
+
+  var insertImage = function(src, desc){
+    // $.markItUp({replaceWith:"!./"+ src +"!"});
+    img = src.replace(/files\//, "");
+    $.markItUp({
+      replaceWith:"!index.php?rex_resize=[![Image Width]!]w__"+ img +"!"
+      });
+  };
+
+  var markitup_getURLParam = function(strParamName){
+    var strReturn = "";
+    var strHref = window.location.href;
+    if ( strHref.indexOf("?") > -1 ){
+      var strQueryString = strHref.substr(strHref.indexOf("?")).toLowerCase();
+      var aQueryString = strQueryString.split("&");
+      for ( var iParam = 0; iParam < aQueryString.length; iParam++ ){
+        if( aQueryString[iParam].indexOf(strParamName.toLowerCase() + "=") > -1 ){
+          var aParam = aQueryString[iParam].split("=");
+          strReturn = aParam[1];
+          break;
+        }
       }
     }
-  }
-  return unescape(strReturn);
-};
-
-jQuery(function($){
+    return unescape(strReturn);
+  };
 
   $(document).on('dblclick','.markItUpFooter', function(e){
     $(e.target).next('.markItUpPreviewFrame').remove();
   });
 
-});
+
+}); // jQuery(function($){ /////////////////////////////////////////////////////
+
+
 
 ;(function ( $, window, document, undefined ) {
 
@@ -110,23 +114,23 @@ jQuery(function($){
                                 // PHRASE MODIFIER
                                 ////////////////////////////////////////////////
                                 'bold':         {
-                                                  openWith:' *',
-                                                  closeWith:'* ',
+                                                  openWith:'*',
+                                                  closeWith:'*',
                                                   key:'B'
                                                 },
                                 'italic':       {
-                                                  openWith:' _',
-                                                  closeWith:'_ ',
+                                                  openWith:'_',
+                                                  closeWith:'_',
                                                   key:'I'
                                                 },
                                 'stroke':       {
-                                                  openWith:' -',
-                                                  closeWith:'- ',
+                                                  openWith:'-',
+                                                  closeWith:'-',
                                                   key:'S'
                                                 },
                                 'ins':          {
-                                                  openWith:' +',
-                                                  closeWith:'+ '
+                                                  openWith:'+',
+                                                  closeWith:'+'
                                                 },
                                 'cite':         {
                                                   openWith:' ??',
@@ -308,6 +312,16 @@ jQuery(function($){
     Plugin.prototype = {
 
         init: function() {                                                                                                       //console.log('this.options:',this.options);
+
+            // (SELECTION.JS) HELPERS
+            var toString = Object.prototype.toString;
+            var isArray = Array.isArray;
+            if (!isArray) {
+              isArray = function(val) {
+                return toString.call(val) === '[object Array]';
+              }
+            }
+
             this.guid = $(this.element).constructor.guid;
 
             if(typeof rex_markitup.i18n === 'undefined') {
@@ -360,6 +374,26 @@ jQuery(function($){
             }
 
             return $(this.element).markItUp({
+              beforeInsert: $.proxy(function(h) {
+                console.group(h.className);
+                  console.group('beforeInsert:');
+                    h.selection = this.selection($(h.textarea));
+                    console.log('selection.text():',h.selection.text());
+                    console.log('selection.surround():',h.selection.surround());
+                    console.log('selection.surround(2):',h.selection.surround(2));
+                    console.log('selection.cursor():',h.selection.cursor());
+                  console.groupEnd();
+              },this),
+              afterInsert: $.proxy(function(h) {
+                  console.group('afterInsert:');
+                    h.selection = this.selection($(h.textarea));
+                    console.log('selection.text():',h.selection.text());
+                    console.log('selection.surround():',h.selection.surround());
+                    console.log('selection.cursor():',h.selection.cursor());
+                  console.groupEnd();
+                console.groupEnd();
+
+              },this),
               nameSpace: this.options.namespace,
               markupSet: this.markupSet,
               previewParserPath: 'index.php?api=rex_markitup_api&func=parse_preview&uid='+this.guid,
@@ -377,6 +411,196 @@ jQuery(function($){
             success: $.proxy(function(data) { rex_markitup.i18n = data; /*console.log('data:',data);*/ },this),
             error: function(e){ console.warn('error:',e); }
           });
+        },
+
+        // GUTTED SELECTION.JS FUNCS FROM HERE ON..
+        ////////////////////////////////////////////////////////////////////////
+        selection: function(inputor) {
+          if (inputor && inputor.length) {
+            // if inputor is jQuery or zepto or a list of elements
+            inputor = inputor[0];
+          }
+          if (inputor) {
+            // detect feature first.
+            if (typeof inputor.selectionStart != 'undefined') {
+              return this.Selection(inputor);
+            }
+            var tag = inputor.tagName.toLowerCase();
+          }
+          if (tag && (tag === 'textarea' || tag === 'input')) {
+            // if has inputor and inputor element is textarea or input
+            return new Selection(inputor, true);
+          }
+
+          if (window.getSelection) return new DocumentSelection();
+          if (document.selection) return new DocumentSelection(true);
+          throw new Error('your browser is very weird');
+        },
+        Selection: function (inputor, isIE) {
+          this.element = inputor;
+          this.cursor = function(start, end) {
+            // get cursor
+            var inputor = this.element;
+            if (typeof start === 'undefined') {
+              if (isIE) {
+                return getIECursor(inputor);
+              }
+              return [inputor.selectionStart, inputor.selectionEnd];
+            }
+
+            // set cursor
+            if (isArray(start)) {
+              var _s = start;
+              start = _s[0];
+              end = _s[1];
+            }
+            if (typeof end === 'undefined') end = start;
+            if (isIE) {
+              setIECursor(inputor, start, end);
+            } else {
+              inputor.setSelectionRange(start, end);
+            }
+            return this;
+          }
+          return this;
+        },
+        // get or set selected text
+        text: function(text, cur) {
+          var inputor = this.element;
+          var cursor = this.cursor();
+          if (typeof text == 'undefined') {
+            return inputor.value.slice(cursor[0], cursor[1]);
+          }
+          return insertText(this, text, cursor[0], cursor[1], cur);
+        },
+        // get the surround words of the selection
+        surround: function(count) {
+          if (typeof count == 'undefined') count = 1;
+          var value = this.element.value;
+          var cursor = this.cursor();
+          var before = value.slice(
+            Math.max(0, cursor[0] - count),
+            cursor[0]
+          );
+          var after = value.slice(cursor[1], cursor[1] + count);
+          return [before, after];
+        },
+        // append text to the end, and select the appended text
+        append: function(text, cur) {
+          var end = this.cursor()[1];
+          return insertText(this, text, end, end, cur);
+        },
+        // prepend text to the start, and select the prepended text
+        prepend: function(text, cur) {
+          var start = this.cursor()[0];
+          return insertText(this, text, start, start, cur);
+        },
+        // append text to the end, and select the appended text
+        append: function(text, cur) {
+          var end = this.cursor()[1];
+          return insertText(this, text, end, end, cur);
+        },
+        // prepend text to the start, and select the prepended text
+        prepend: function(text, cur) {
+          var start = this.cursor()[0];
+          return insertText(this, text, start, start, cur);
+        },
+        line: function() {
+          var value = this.element.value;
+          var cursor = this.cursor();
+          var before = value.slice(0, cursor[0]).lastIndexOf('\n');
+          var after = value.slice(cursor[1]).indexOf('\n');
+
+          // we don't need \n
+          var start = before + 1;
+          if (after === -1) {
+            return value.slice(start);
+          }
+          var end = cursor[1] + after;
+          return value.slice(start, end);
+        },
+        // Selection on document
+        // TODO: should it support this feature ?
+        DocumentSelection: function(isIE) {
+          if (!isIE) {
+            var sel = window.getSelection();
+            this.element = getSelectionElement(sel);
+            this.text = function() {
+              // TODO set text
+              return sel.toString();
+            }
+          } else {
+            this.text = function() {
+              return document.selection.createRange().text;
+            }
+          }
+          return this;
+        },
+        // IE sucks. This is how to get cursor position in IE.
+        // Thanks to [ichord](https://github.com/ichord/At.js)
+        getIECursor: function (inputor) {
+          var range = document.selection.createRange();
+          if (range && range.parentElement() === inputor) {
+            var start, end;
+            var normalizedValue = inputor.value.replace(/\r\n/g, '\n');
+            var len = normalizedValue.length;
+            var textInputRange = inputor.createTextRange();
+            textInputRange.moveToBookmark(range.getBookmark());
+            var endRange = inputor.createTextRange();
+            endRange.collapse(false);
+            if (textInputRange.compareEndPoints('StartToEnd', endRange) > -1) {
+              start = end = len;
+            } else {
+              start = -textInputRange.moveStart('character', -len);
+              end = -textInputRange.moveEnd('character', -len);
+            }
+            // when select to the last character, end = 1
+            if (end < start) {
+              end = len;
+            }
+            return [start, end];
+          }
+          return [0, 0];
+        },
+        setIECursor: function (inputor, start, end) {
+          var range = inputor.createTextRange();
+          range.move('character', start);
+          // why should it be named as ``moveEnd`` ?
+          range.moveEnd('character', end - start);
+          range.select();
+        },
+        insertText: function (selection, text, start, end, cursor) {
+          if (typeof text == 'undefined') text = '';
+          var value = selection.element.value;
+          selection.element.value = [
+            value.slice(0, start), text, value.slice(end)
+          ].join('');
+          end = start + text.length;
+          if (cursor === 'left') {
+            selection.cursor(start);
+          } else if (cursor === 'right') {
+            selection.cursor(end);
+          } else {
+            selection.cursor(start, end);
+          }
+          return selection;
+        },
+        getSelectionElement: function (sel) {
+          // start point and end point maybe in the different elements.
+          // then we find their common father.
+          var element = null;
+          var anchorNode = sel.anchorNode;
+          var focusNode = sel.focusNode;
+          while (!element) {
+            if (anchorNode.parentElement === focusNode.parentElement) {
+              element = focusNode.parentElement;
+              break;
+            } else {
+              anchorNode = anchorNode.parentElement;
+              focusNode = focusNode.parentElement;
+            }
+          }
+          return element;
         }
 
     };

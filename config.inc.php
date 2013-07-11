0d9e397dce2729e657cdefe0cd7e962a81e75807
jdlx <github@rexdev.de>
2013-07-11 03:14:03 +0200

    add EP "REX_MARKITUP_IMAGE_TYPES_QUERY"

--- config.inc.php
+++ config.inc.php
@@ -19,6 +19,13 @@
 #  }
 #);
 
+#rex_register_extension('REX_MARKITUP_IMAGE_TYPES_QUERY',
+#  function($params) use($REX)
+#  {
+#    return 'SELECT * FROM '.$REX['TABLE_PREFIX'].'679_types WHERE `name` NOT LIKE "rex_%" ORDER BY `name` ASC';
+#  }
+#);
+
 
 // PLUGIN IDENTIFIER & ROOT
 ////////////////////////////////////////////////////////////////////////////////
@@ -184,7 +191,8 @@ function rex_markitup_imm_imgtypes()
   global $REX;
   $REX['ADDON']['image_manager']['types'] = array();
   $db = rex_sql::factory();
-  foreach($db->getArray('SELECT * FROM '.$REX['TABLE_PREFIX'].'679_types ORDER BY `name` ASC') as $type) {
+  $query = rex_register_extension_point('REX_MARKITUP_IMAGE_TYPES_QUERY', 'SELECT * FROM '.$REX['TABLE_PREFIX'].'679_types ORDER BY `name` ASC');
+  foreach($db->getArray($query) as $type) {
     $REX['ADDON']['image_manager']['types'][$type['name']] = $type['description'];
   }
 }

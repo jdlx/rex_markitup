1912425e02ee83afbfc68d9ea84016008074e76b
jdlx <github@rexdev.de>
2013-08-30 08:52:14 +0200

    fix/extend install checks..

--- install.inc.php
+++ install.inc.php
@@ -10,14 +10,56 @@
 
 $myself = 'rex_markitup';
 
-// REQUIRE TEXTILE
+
+// INSTALL CONDITIONS
 ////////////////////////////////////////////////////////////////////////////////
-if(!isset($ADDONSsic['version']['textile']))
+$requiered_REX    = '4.4.0';
+$requiered_PHP    = '5.3.0';
+$requiered_addons = array('textile');
+
+
+// CHECK REDAXO VERSION
+////////////////////////////////////////////////////////////////////////////////
+if(version_compare($REX['VERSION'].'.'.$REX['SUBVERSION'].'.'.$REX['MINORVERSION'], $requiered_REX, '<'))
 {
-  $REX['ADDON']['installmsg'][$myself] = 'Textile Addon required!';
+  $REX['ADDON']['installmsg'][$myself] = 'Dieses Addon ben&ouml;tigt Redaxo Version '.$requiered_REX.' oder h&ouml;her.';
   $REX['ADDON']['install'][$myself] = 0;
   return;
 }
+
+
+// CHECK PHP VERSION
+////////////////////////////////////////////////////////////////////////////////
+if(version_compare(PHP_VERSION, $requiered_PHP, '<'))
+{
+  $REX['ADDON']['installmsg'][$myself] = 'Dieses Addon ben&ouml;tigt mind. PHP '.$requiered_PHP.'!';
+  $REX['ADDON']['install'][$myself] = 0;
+  return;
+}
+
+
+// CHECK REQUIERED ADDONS
+////////////////////////////////////////////////////////////////////////////////
+foreach($requiered_addons as $a)
+{
+  if (!OOAddon::isInstalled($a))
+  {
+    $REX['ADDON']['installmsg'][$myself] = '<br />Addon "'.$a.'" ist nicht installiert.  >>> <a href="index.php?page=addon&addonname='.$a.'&install=1">jetzt installieren</a> <<<';
+    $REX['ADDON']['install'][$myself] = 0;
+    return;
+  }
+  else
+  {
+    if (!OOAddon::isAvailable($a))
+    {
+      $REX['ADDON']['installmsg'][$myself] = '<br />Addon "'.$a.'" ist nicht aktiviert.  >>> <a href="index.php?page=addon&addonname='.$a.'&activate=1">jetzt aktivieren</a> <<<';
+      $REX['ADDON']['install'][$myself] = 0;
+      return;
+    }
+
+  }
+}
+
 
 
 $REX['ADDON']['install'][$myself] = 1;

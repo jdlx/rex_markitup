86788882933972eecd7eb293884fb6d119d04461
jdlx <github@rexdev.de>
2014-08-27 11:21:35 +0200

    version bump -> 1.0.7

--- install.inc.php
+++ install.inc.php
@@ -2,7 +2,7 @@
 /**
  * RexMarkitup for Redaxo
  *
- * @version 1.0.6
+ * @version 1.0.7
  * @link http://markitup.jaysalvat.com
  * @author Redaxo Addon: rexdev.de
  * @package redaxo 4.4.x/4.5.x/4.6.x
